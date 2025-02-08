10092d3fdf05bbbb82111e3bf617b4bc288cab3e
user <fldpi@tutanota.com>
2025-02-08 20:24:23 +0300

    Initial release

---
+++ Source/CompRefuelableElectric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace ExtraShipParts
+{
+	class CompRefuelableElectric : CompRefuelable
+	{
+		public CompPowerBattery battery;
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			battery = parent.GetComp<CompPowerBattery>();
+		}
+		public override void CompTick()
+		{
+			base.CompTick();
+			if (battery == null)
+			{
+				return;
+			}
+			if (Find.TickManager.TicksGame % 30 == 0)
+			{
+				this.GetType().GetField("fuel", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(this, battery.StoredEnergy);
+			}
+		}
+	}
+}
---
+++ Source/Main.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExtraShipParts
+{
+	[StaticConstructorOnStartup]
+	public class Main
+	{
+		static Main()
+		{
+			Harmony pat = new Harmony("ExtraShipParts");
+			pat.PatchAll();
+		}
+	}
+
+	// Electric engine (in Tiny submod) has 2 power comps, so need to disable post spawn setup for one of those
+	[HarmonyPatch(typeof(CompPowerBattery), "PostExposeData")]
+	public static class DoublePowerCompOnElectricEngineFix
+	{
+		public static bool Prefix(CompPower __instance)
+		{
+			if (__instance.parent.def.defName == "Ship_Engine_Electric_Tiny")
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+
+
+}
+
